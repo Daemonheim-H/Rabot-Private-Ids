1c7b86ae5b35976694a6c6bbada246bf596d8b6f
OfficiallyNebula <56564007+OfficiallyNebula@users.noreply.github.com>
2020-11-25 21:16:39 +0000

    Added More AObjectID's
    - Nebula

--- AobjectID.cs
+++ AobjectID.cs
@@ -32,11 +32,85 @@
         public const int CLAY_ROCK = 113154;
         public const int CLAY_ROCK_2 = 113152;
 
+        public const int GOLD_ROCK = 113059;
+        public const int GOLD_ROCK_2 = 113061;
+        public const int GOLD_ROCK_3 = 113060;
+
+        public const int MITHRIL_ROCK = 113052;
+        public const int MITHRIL_ROCK_2 = 113050;
+        public const int MITHRIL_ROCK_3 = 113051;
+
+        public const int ADAMNTITE_ROCK = 113055;
+        public const int ADAMNTITE_ROCK_1 = 11053;
+
+        public const int SILVER_ROCK = 113046;
+        public const int SILVER_ROCK_2 = 113045;
+        public const int SILVER_ROCK_3 = 113044;
+
+        public const int COMMON_GEM_ROCK = 113036;
+        public const int COMMON_GEM_ROCK_2 = 113037;
+
+        public const int UNCOMMON_GEM_ROCK = 113047;
+        public const int UNCOMMON_GEM_ROCK_2 = 113048;
+        public const int UNCOMMON_GEM_ROCK_3 = 113049;
+
+        public const int PRECIOUS_GEM_ROCK = 113186;
+        public const int PRECIOUS_GEM_ROCK_2 = 113187;
+
+        public const int IRON_ROCK = 113100;
+        public const int IRON_ROCK_2 = 113099;
+
+        public const int BLURITE_ROCK = 112896;
+        public const int BLURITE_ROCK_2 = 112897;
+        public const int BLURITE_ROCK_3 = 112898;
+
+        public const int DRAKOLITH_ROCK = 113196;
+        public const int DRAKOLITH_ROCK_2 = 113195;
+        public const int DRAKOLITH_ROCK_3 = 113194;
+
 
         // Archeology
         public const int VENATOR_REMAINS = 117100;
         public const int PYLON = 116957;
         public const int MATERIAL_STORAGE_CONTAINER = 116439;
+
+        
+        // WoodCutting
+        public const int TREE = 38785;
+        public const int TREE_1 = 39145;
+        public const int TREE_2 = 38795;
+        public const int TREE_3 = 38760;
+        public const int TREE_4 = 40313;
+        public const int TREE_5 = 38783;
+        public const int TREE_6 = 38732;
+        public const int TREE_7 = 39121;
+
+        public const int IVY = 86563;
+        public const int IVY_1 = 86561;
+        public const int IVY_2 = 86564;
+        public const int IVY_3 = 86562;
+        
+        public const int YEW = 38759;
+        public const int YEW_1 = 38758;
+        public const int YEW_2 = 38755;
+
+        public const int OAK = 38736;
+        public const int OAK_1 = 38731;
+        public const int OAK_2 = 38739;
+        public const int OAK_3 = 38732;
+
+
+        // Farming
+        public const int BUSH_PATCH = 7577;
+        public const int TREE_PATCH = 8389;
+
+
+        // MISC - Write comments after each line to explain what it does.
+        public const int PIPE = 113238; // PIPE_(Number) = Repairable pipes in Falador smithing guild
+        public const int PIPE_2 = 113233; // PIPE_(Number) = Repairable pipes in Falador smithing guild
+        public const int PIPE_3 = 113253; // PIPE_(Number) = Repairable pipes in Falador smithing guild
+        public const int PIPE_4 = 113248; // PIPE_(Number) = Repairable pipes in Falador smithing guild
+        public const int PIPE_5 = 113243; // PIPE_(Number) = Repairable pipes in Falador smithing guild
 
     }
 }
